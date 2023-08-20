02be0a4063555a2683f5cea5fdb32cd6bf65e393
Ridwan <aladebaruwa@gmail.com>
2023-08-20 18:12:11 +0100

    add

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ let userAllowed;
 let age = prompt('How old are you?', ' ');
 
 if (age > 5) {
-    userAllowed = true;
+    userAllowed = false;
     alert(`Acces Allowed status: ${userAllowed}`)
 }
 else if (age > 10) {
